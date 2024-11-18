38c6e44ce7ae111f7554a63b7ee830d8681035cc
Kevin <kevin.shehan30@gmail.com>
2024-11-18 22:56:19 +0530

    mongodb configuration

--- index.mjs
+++ index.mjs
@@ -1,1 +1,21 @@
 import mongoose from "mongoose";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+const app = express();
+app.listen(PORT, () => {
+    console.log(`Server is Running on port ${PORT}`)
+})
+
+// Connect to MongoDB
+mongoose.connect("localhost:27100/smc",)
+.then(() => {
+    console.log("Connected to MongoDB")
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB")
+})
