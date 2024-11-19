4ab013852bc312abb2a94e2708b35f5f442e666b
Kevin <kevin.shehan30@gmail.com>
2024-11-19 15:20:16 +0530

    2. Login

--- index.mjs
+++ index.mjs
@@ -112,3 +112,22 @@ app.post('/register', upload.single('image'), async (req, res) => {
       res.status(400).json({ error: err.message });
     }
   });
+
+  // 2. Login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      const user = await User.findOne({ email });
+      if (!user) return res.status(404).json({ message: 'User not found' });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+  
+      const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, { expiresIn: '1h' });
+      res.json({ token });
+    } catch (err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+  
