b282dfbd43e3835c6fc8afa3b7b1f4613e9cc49e
Kevin <kevin.shehan30@gmail.com>
2024-11-19 15:16:10 +0530

    User Schema pre hash

--- index.mjs
+++ index.mjs
@@ -49,3 +49,15 @@ const userSchema = new mongoose.Schema({
     imagePath: String,
     qrCodePath: String,
 });
+
+// Password encryption
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+      const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+});
+const User = mongoose.model('User', userSchema);
+
+// Generate QR code
