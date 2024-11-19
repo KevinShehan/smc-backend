0926561c0ce25fa061700fef03e8daccee9df220
Kevin <kevin.shehan30@gmail.com>
2024-11-19 15:04:26 +0530

    jwt secret key added

--- index.mjs
+++ index.mjs
@@ -16,6 +16,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
+const JWT_SECRET = process.env.JWT_SECRET || 'supersecretkey';
 
 const app = express();
 
