a62dea566cbf575e84e7ab5ab8e684a004c7a5df
Kevin <kevin.shehan30@gmail.com>
2024-11-19 10:55:03 +0530

    user model modified

--- models/User.model.js
+++ models/User.model.js
@@ -2,5 +2,15 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const UserSchema = mongoose.model("UserSchema",{
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+    image: { type: String }, // Path to uploaded image
+    qrCode: { type: String }, // Path to QR code
 
+},{
+    timestamps: true
 });
+
+export default mongoose.model('User', userSchema);
